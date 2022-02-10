de78f942e05595a12371e117140b8471faf9fe4c
badriyvp <pavankumarreddyalavala@gmail.com>
2022-02-10 23:28:41 +0530

    small project code

--- 3_Implementation/programm.c
+++ 3_Implementation/programm.c
@@ -9,35 +9,46 @@
  * 
  */
 
-
-
-
-
 #include<stdio.h>
 int main()
 {
-int runs, wickets, score;
+int runs, wickets, score, player;
 scanf("%d",&runs);
 scanf("%d",&wickets);
 
 score=  scoreboard(runs,wickets);
+player= name(&runs,&wickets);
+
 
 }
 int scoreboard(int r1, int w1)
 {
     {
 if (r1>100)
-printf("century\n");
+printf("century good match\n");
 else if(r1<1)
 printf("duckout\n");
     }
     {
     if(w1>=5)
-    printf("fivewickets");
+    printf("fivewickets great bolwer\n");
+    else if(w1<=1)
+    printf("most expensive bolwer\n");
 
+}
+}
+int name(int *r2,int *w2)
+
+{
+ if(*r2>50)
+ printf("namee of player is virat\n");
+ else
+ printf("others\n");
+    
 }
 
 
 
-}
+
+
 
